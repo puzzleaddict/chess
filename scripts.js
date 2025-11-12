d288e65318e1c7a94eeb2cfbee9267b511f19249
PuzzleAddict <puzzle@addict.com>
2025-11-12 20:40:20 +1100

    Style for different screen sizes.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// TODO: Provide alternate piece sets
+
 const files = ['a','b','c','d','e','f','g','h'];
 const ranks = ['8','7','6','5','4','3','2','1'];
 const grid = document.getElementById('grid');
